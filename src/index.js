87b92e6d7a8debd0894538398ab77751764de9f4
Bartosz Kowalski <b.kowalski@visuality.pl>
2020-05-04 19:22:51 +0200

    feat(realease): new version

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 /**
  * Stylelint your code with Danger
  */
-import stylelint from 'stylelint';
+import stylelint from 'stylelint'
 
 export default async function stylelint() {
-  
+  const filesToLint = danger.git.created_files.concat(danger.git.modified_files)
+  return Promise.all(
+    filesToLint.filter(f => /css$/.test(f)).map(f => lintFile(cli, config, f)),
+  )
+}
+
+async function lintFile(path) {
+  stylelint.lint({ files: path }).then(data => {
+    if (data.errored === false) {
+      return
+    }
+
+    data.results[0].warnings.map(result => {
+      warn(result.text, path, result.line)
+    })
+  })
 }
